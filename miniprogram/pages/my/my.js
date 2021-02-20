e0b3a3cb6762e5ebb8204d715bd3b5e6b7559b07
cl1997 <leiloloaa@gmail.com>
2021-02-20 16:39:08 +0800

    init

---
+++ miniprogram/pages/my/my.js
@@ -0,0 +1,41 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+    data: {
+        motto: 'Hello World',
+        userInfo: {},
+        hasUserInfo: false,
+        canIUse: wx.canIUse('button.open-type.getUserInfo'),
+        modalName: ''
+    },
+    onLoad: function() {
+        // 获取用户信息
+        wx.getSetting({
+            success: res => {
+                if (res.authSetting['scope.userInfo']) {
+                    // 已经授权，可以直接调用 getUserInfo 获取头像昵称，不会弹框
+                    wx.getUserInfo({
+                        success: res => {
+                            this.setData({
+                                avatarUrl: res.userInfo.avatarUrl,
+                                userInfo: res.userInfo
+                            })
+                        }
+                    })
+                }
+            }
+        })
+    },
+    showModal(e) {
+        this.setData({
+            modalName: e.currentTarget.dataset.target
+        })
+    },
+    hideModal(e) {
+        this.setData({
+            modalName: null
+        })
+    },
+})
